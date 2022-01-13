ea5e9a519e4b6637754df50343589bf0b729a533
B <77552838+Mrballer59@users.noreply.github.com>
2022-01-13 14:46:37 +0000

    index page part

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,39 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const link = document.createElement("a");
+    link.setAttribute("href", "./photographer.html");
+    link.setAttribute("aria-label", name);
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", "");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    // article.appendChild(img);
+    // article.appendChild(h2);
+    const pLocation = document.createElement("p");
+    pLocation.textContent = `${city},${country}`;
+
+    const pTagline = document.createElement("p");
+    pTagline.textContent = `${tagline}`;
+
+    const pPrice = document.createElement("p");
+    pPrice.textContent = `${price}€/jour`;
+
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(pLocation);
+    article.appendChild(pTagline);
+    article.appendChild(pPrice);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
