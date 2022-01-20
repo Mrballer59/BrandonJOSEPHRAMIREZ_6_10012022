1393285aa5d5fd422fd367fcef20e8c07f8d2eea
B <77552838+Mrballer59@users.noreply.github.com>
2022-01-20 18:48:49 +0000

    Font + p colors display header need to fix idphoto

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,11 +1,46 @@
 //Mettre le code JavaScript lié à la page photographer.html
-function getPhotographers() {
+async function getPhotographers() {
   let photographers = [];
-  fetch("./data/photographers.json")
+  await fetch("./data/photographers.json")
     .then((res) => res.json())
     .then((data) => (photographers = data.photographers));
+
   return {
     photographers,
   };
 }
 getPhotographers();
+
+async function getMedias() {
+  let media = [];
+  await fetch("./data/photographers.json")
+    .then((res) => res.json())
+    .then((data) => (media = data.media));
+
+  return {
+    media,
+  };
+}
+getMedias();
+
+async function displayData(photographers) {
+  // Get the id in l'URL
+  const photographersHeader = document.querySelector(".photograph-header");
+  const PageQueryString = window.location.search;
+  const urlPart = new URLSearchParams(PageQueryString);
+  const idPage = urlPart.get("id");
+
+  const profile = photographers.find((element) => element.id == idPage);
+
+  const photographersProfile = profileFactory(profile);
+  const userCardDOM = photographersProfile.getUserCardDOM();
+  photographersHeader.appendChild(userCardDOM);
+}
+
+async function init() {
+  // fetchs the data and display it
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
